af81b49dcd2b1161c9eb22393f9a875b37399b3c
Dennis Lustre <dennis.lustre@gmail.com>
2024-04-30 14:51:45 -0700

    simplify bluetooth code

--- master.ino
+++ master.ino
@@ -1,37 +1,15 @@
-/*
- * How to configure and pair two HC-05 Bluetooth Modules
- * by Dejan Nedelkovski, www.HowToMechatronics.com
- *
- *                 == MASTER CODE ==
- */
-
 #define ledPin 9
 
 int state = 0;
 int potValue = 0;
 
-void setup() {
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
-  Serial.begin(38400); // Default communication rate of the Bluetooth module
-}
+void setup() { Serial.begin(38400); }
 
 void loop() {
-  if (Serial.available() >
-      0) { // Checks whether data is comming from the serial port
-    state = Serial.read(); // Reads the data from the serial port
+  if (Serial.available() > 0) {
+    state = Serial.read();
+    Serial.println(state);
   }
-  // Controlling the LED
-  if (state == '1') {
-    digitalWrite(ledPin, HIGH); // LED ON
-    state = 0;
-  } else if (state == '0') {
-    digitalWrite(ledPin, LOW); // LED ON
-    state = 0;
-  }
-  // Reading the potentiometer
-  potValue = analogRead(A0);
-  int potValueMapped = map(potValue, 0, 1023, 0, 255);
-  Serial.write(potValueMapped); // Sends potValue to servo motor
-  delay(10);
+  Serial.write(42);
+  delay(1000);
 }
